ef3d041b3181b41bec64fb62ddab620eefc25f57
Jamie Miles <jamie.miles@weston.ac.uk>
2023-03-02 20:16:48 +0000

    Add project files.

---
+++ C_Sharpe_Error_Code/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace C_Sharpe_Error_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
